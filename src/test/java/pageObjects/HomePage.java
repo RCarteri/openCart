4236f9cebc1bbd074954ab0b1052e561439f6bb7
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2023-03-14 11:29:47 -0300

    Login Test, Data Driven

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -5,23 +5,36 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
-    public HomePage(WebDriver driver) {
-        super(driver);
-    }
 
-    // Elements
-    @FindBy(xpath = "//span[text()='My Account']")
-    WebElement lnkMyaccount;
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
 
-    @FindBy(linkText = "Register")
-    WebElement lnkRegister;
+	// Elements
+	@FindBy(xpath = "//span[text()='My Account']")
+	WebElement lnkMyaccount;
 
-    // Action Methods
-    public void clickMyAccount() {
-        lnkMyaccount.click();
-    }
+	@FindBy(linkText = "Register")
+	WebElement lnkRegister;
 
-    public void clickRegister() {
-        lnkRegister.click();
-    }
+	@FindBy(linkText = "Login")   // Login link added in step6
+	WebElement linkLogin;
+	
+		
+	// Action Methods
+	public void clickMyAccount() {
+		lnkMyaccount.click();
+	}
+
+	public void clickRegister() {
+		lnkRegister.click();
+	}
+	
+	public void clickLogin()    // added in step6
+	{
+		linkLogin.click();
+	}
+	
+	
+	
 }
